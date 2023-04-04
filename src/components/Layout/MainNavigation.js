c862f23ff5c9412a268ef5cf72fbaa4a56560fe5
roshankhaire <roshankhaire1992@gmail.com>
2023-04-04 16:18:40 +0530

    Addressing security and logout issues

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,16 +1,15 @@
 import { Link } from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
 import { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 const MainNavigation = () => {
-  const history=useHistory();
+  
   const authCtx=useContext(AuthContext);
   const isLoggedIn=authCtx.isLoggedIn;
   const logoutHandler=()=>{
     authCtx.logout();
    
-    history.replace('/')
+    
   }
   return (
     <header className={classes.header}>
